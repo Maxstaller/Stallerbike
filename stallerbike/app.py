47eb531213ca1eb143a79f49008b4740d9725d95
Maxstaller <max.stallerbike@yahoo.com>
2025-11-10 12:16:34 +0000

    Update app.py

--- stallerbike/app.py
+++ stallerbike/app.py
@@ -257,3 +257,6 @@ with app.app_context():
 # App-Start (nur lokal relevant)
 if __name__ == '__main__':
     app.run(debug=True)
+print("REGISTERED ROUTES:")
+for rule in app.url_map.iter_rules():
+    print(rule)
